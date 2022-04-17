06b3f48fa2bb42b119269d6c872693a7678aa913
walidksb <walid.kesbi@gmail.com>
2022-04-18 00:08:57 +0100

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,25 @@
 let navButton = document.getElementById('nav-icon');
 let linksRow = document.getElementById('links-row');
 
+if (window.innerWidth > 864){
+    shouldApear = true;
+} else shouldApear = false;
+
+const updateManue = () => {
+    if (shouldApear) linksRow.style.display = 'flex';
+    else linksRow.style.display = 'none';
+}   
+
 navButton.addEventListener('click', () => {
-    linksRow.style.display = 'flex';
+    shouldApear = !shouldApear;
+    updateManue();
 });
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 864){
+        shouldApear = true;
+        updateManue();
+    }
+})
+
+updateManue();
